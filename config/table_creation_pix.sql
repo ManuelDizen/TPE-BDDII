fb3349cce72b0b807f5994b53ef06aad52551c51
Manuel <mdizenhaus@itba.edu.ar>
2023-07-02 17:02:55 -0300

    small script for table creation

---
+++ config/table_creation_pix.sql
@@ -0,0 +1,27 @@
+create table if not exists banks(
+    id serial primary key,
+    code varchar(4) not null
+);
+
+create table if not exists bank_accounts(
+    id serial primary key,
+    balance integer not null,
+    bank_id integer not null,
+    cbu varchar not null,
+    foreign key (bank_id) references banks(id)
+);
+
+create table if not exists users(
+    id serial primary key,
+    cuit varchar not null,
+    email varchar,
+    name varchar,
+    phone varchar
+);
+
+create table if not exists user_to_banks(
+    id serial primary key,
+    user_id int not null,
+    bank_id int not null,
+    unique(user_id, bank_id)
+);
