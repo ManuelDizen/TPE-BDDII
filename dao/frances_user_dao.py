5329fc18cbf3251c0584b97ad6dc887c8b80ebea
Manuel <mdizenhaus@itba.edu.ar>
2023-07-04 11:43:13 -0300

    depositing endpoint working

--- dao/frances_user_dao.py
+++ dao/frances_user_dao.py
@@ -1,6 +1,8 @@
 import couchdb2
 from couchdb2 import Database
 from os import getenv
+import requests
+import json
 
 # Codigo de banco 017
 
@@ -22,8 +24,59 @@ class FrancesUserDao:
         self.db.put(user)
         return 
     
-    def find_user_by_cbu(self, cbu:str):
-        return
+    def get_user_by_cbu(self, cbu:str):
+        query = {
+            'selector': {
+                "cbu": cbu
+            },
+            'limit': 1
+}
+        response = requests.post(
+            'http://admin:tpebdd2@localhost:5984/frances_users/_find', json=query
+        ) #TODO: Method that creates this url based on env variables
+
+        json_response = json.loads(response.text)
+        #print(json_response)
+        return json_response
+    
+    def extract_from_account(self, cbu:str, amount:int):
+        user = self.get_user_by_cbu(cbu)
+        if "cbu" not in user["docs"][0]:
+            return 404
+        print(user["docs"][0])
+        current_balance = user["docs"][0]["balance"]
+
+        if current_balance < amount:
+            return 400 #Not enough funds
+        
+        user["docs"][0]["balance"] = current_balance - amount
+        print("Modified user: ")
+        print(user["docs"][0])
+        
+    def deposit_to_account(self, cbu:str, amount:int):
+        user = self.get_user_by_cbu(cbu)
+        if "cbu" not in user["docs"][0]:
+            return 404
+        current_balance = user["docs"][0]["balance"]
+        new_balance = current_balance + amount
+        body = {
+            "name":user["docs"][0]["name"],
+            "cbu":user["docs"][0]["cbu"],
+            "balance":new_balance, 
+            "_rev":user["docs"][0]["_rev"]
+            }
+        print(body)
+        url = 'http://admin:tpebdd2@localhost:5984/frances_users/{}'.format(
+            user["docs"][0]["_id"]
+        )
+
+        print(url)
+        response = requests.put(url, json.dumps(body))
+        json_response = json.loads(response.text)
+        print(json_response)
+
+        if "error" in json_response:
+            return -1
 
     def get_base_cbu(self):
         return self.base_cbu_block
--- routes/frances_user_router.py
+++ routes/frances_user_router.py
@@ -20,3 +20,62 @@ async def create_user(
         status_code=201,
         #TODO: location
     )
+
+@router.get(
+    "/{cbu}",
+    status_code=200,
+    responses={
+        404: {"description":"not found"},
+    },
+)
+async def get_user_by_cbu(
+    cbu:str, 
+    frances_user_dao: FrancesUserDao = Depends(get_frances_user_dao)
+):
+    user = frances_user_dao.get_user_by_cbu(cbu)
+    if "cbu" not in user["docs"][0]:
+        raise HTTPException(404, "not found")
+    return user
+
+@router.patch(
+    '/{cbu}/deposit',
+    status_code=200,
+    responses={
+        404:{"description":"user doesn't exist"},}
+)
+async def deposit_to_account(
+    cbu:str,
+    amount:int,
+    request: Request,
+    frances_user_dao: FrancesUserDao = Depends(get_frances_user_dao)
+):
+    check = frances_user_dao.deposit_to_account(cbu, amount)
+    if check == 404:
+        raise HTTPException(404, "Account does no texist")
+    elif check == -1:
+        raise HTTPException(400, "error en http request")
+    return Response(
+        status_code=200
+    )
+
+@router.patch(
+    '/{cbu}/extract',
+    status_code=200,
+    responses={
+        404:{"description":"user doesn't exist"},
+        409:{"description":"Not enough funds"}}
+)
+async def extract_from_account(
+    cbu:str,
+    amount:int,
+    request: Request,
+    frances_user_dao: FrancesUserDao = Depends(get_frances_user_dao)
+):
+    check = frances_user_dao.extract_from_account(cbu, amount)
+    if check == 400:
+        raise HTTPException(400, "Not enough funds")
+    if check == -1:
+        raise HTTPException(400, "Error in extraction")
+    return Response(
+        status_code=200
+    )
